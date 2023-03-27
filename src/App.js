6e44706ec689ee6bbb107f7413a608cf86683fcf
ockap <62844331+slewinus@users.noreply.github.com>
2023-03-27 16:00:53 +0200

    3

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import gif from './gif.gif';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={gif} alt="gif"/>
-        <p>
-            Bon ok j'ai compris comment ça marche a peut prêt mdr
-        </p>
-      </header>
-    </div>
-  );
+    const [gifs, setGifs] = useState([]);
+
+    useEffect(() => {
+        // Tableau de liens vers les GIFs
+        const gifLinks = [
+            'https://i.gifer.com/2GU.gif',
+            'https://i.gifer.com/VZmo.gif',
+            'https://i.gifer.com/5GJg.gif',
+        ];
+
+        // Générer 3 index aléatoires
+        const randomIndexes = [];
+        while (randomIndexes.length < 1) {
+            const index = Math.floor(Math.random() * gifLinks.length);
+            if (!randomIndexes.includes(index)) {
+                randomIndexes.push(index);
+            }
+        }
+
+        // Récupérer les liens correspondant aux index aléatoires
+        const randomGifs = randomIndexes.map(index => gifLinks[index]);
+
+        setGifs(randomGifs);
+    }, []);
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                {gifs.map((gif, index) => (
+                    <img key={index} src={gif} alt="GIF animé" />
+                ))}
+                <p>
+                    Bon ok j'ai compris comment ça marche à peu près mdr
+                </p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
