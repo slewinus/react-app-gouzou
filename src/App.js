36873efdb107c96a5fa026b3f78b8bf977d2b28f
ockap <62844331+slewinus@users.noreply.github.com>
2023-03-28 10:46:07 +0200

    ip add

--- src/App.js
+++ src/App.js
@@ -3,13 +3,22 @@ import './App.css';
 
 function App() {
     const [gifs, setGifs] = useState([]);
+    const [ipAddress, setIpAddress] = useState('');
+
 
     useEffect(() => {
-        // Tableau de liens vers les GIFs
+        fetch('https://api.ipify.org?format=json')
+            .then((response) => response.json())
+            .then((data) => setIpAddress(data.ip))
+            .catch((error) => console.log(error));
+
+
+       // Tableau de liens vers les GIFs
         const gifLinks = [
             'https://i.gifer.com/2GU.gif',
             'https://i.gifer.com/VZmo.gif',
             'https://i.gifer.com/5GJg.gif',
+            'https://media.giphy.com/media/Rlwz4m0aHgXH13jyrE/giphy.gif'
         ];
 
         // Générer 3 index aléatoires
@@ -33,6 +42,8 @@ function App() {
                 {gifs.map((gif, index) => (
                     <img key={index} src={gif} alt="GIF animé" />
                 ))}
+                <h1>Adresse IP de l'utilisateur :</h1>
+                <p>{ipAddress}</p>
                 <p>
                     Bon ok j'ai compris comment ça marche à peu près mdr
                 </p>
